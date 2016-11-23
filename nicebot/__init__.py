2a44cd4e7cc736890e27d14d7c637319d18e9868
ctaylor08 <taylor.cory08@gmail.com>
2016-11-22 23:20:41 -0500

    Stream of tweets with offensive words

--- nicebot/__init__.py
+++ nicebot/__init__.py
@@ -1,6 +1,8 @@
 #usr/bin/python3
 import os
 import configparser
+import json
+import time
 from tweepy import Stream, OAuthHandler
 from tweepy.streaming import StreamListener
 
@@ -11,8 +13,12 @@ twitter_auth = config['twitter_auth']
 class Listener(StreamListener):
 
     def on_data(self, data):
-        print(data)
-        return True
+        all_data = json.loads(data)
+        tweet = all_data.get('text')
+        if tweet and not tweet.startswith('RT '):
+            username = all_data['user']['screen_name']
+            print('{} tweeted:\n\t{}\n'.format(username, tweet))
+            return True
         
     def on_error(self, status):
         print(status)
@@ -21,4 +27,19 @@ auth = OAuthHandler(twitter_auth['ckey'], twitter_auth['csecret'])
 auth.set_access_token(twitter_auth['atkn'], twitter_auth['asecret'])
 
 twitterStream = Stream(auth, Listener())
-twitterStream.filter(track=['car'])
+def enable():
+    try:
+        twitterStream.filter(track=['cunt', 'nigger', 'spic', 'kike', 'felch'])
+    except KeyboardInterrupt:
+        print('\nnicebot disabled')
+        
+if __name__ == '__main__':
+    print("Let's kill em with kindness!")
+    print("- press Ctrl+c to disable at anytime")
+    print("- Starting in")
+    s = 1
+    for sec in ['5','4','3','2','1']:
+        print(' '*s + sec)
+        s += 1
+        time.sleep(1)
+    enable()
